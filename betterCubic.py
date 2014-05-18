962d701a2e1ab1acbf76a437249115fe66762987
Santiago Paiva <paiva.santiago@gmail.com>
2014-05-17 22:43:32 -0400

    Update betterCubic.py

--- betterCubic.py
+++ betterCubic.py
@@ -1,27 +1,72 @@
+import math
+
+class Cubic: 
+
+"""
+    Constructor
+"""
+  def __init__(self, a, b, c, d):
+    self.a = a
+    self.b = b
+    self.c = c
+    self.d = d
+
+"""
+    getRoots Method
+"""
   def getRoots(self):
-    self._f()
-    self._g()
-    self._h()
+    
+    if self.f == 0 and self.g == 0 and self.h == 0:
+      x1, x2, x3 = [-(self.d/self.a)**(1/3)]*3
+
+    elif self.h > 0:
+      x1 = (self.s+self.u)-(self.b/(3*self.a))
+      x2 = complex(-(self.s+self.u)/2 - (self.b/(3*self.a)),
+                   ((self.s-self.u)*3**0.5)/2)
+      x3 = complex(-(self.s+self.u)/2 - (self.b/(3*self.a)),
+                   -((self.s-self.u)*3**0.5)/2)
+
+    else:
+      x1 = 2*self.j*math.cos(self.k/3)-(self.b/(3*self.a))
+      x2 = -self.j*(self.m+self.n)+self.p
+      x3 = -self.j*(self.m-self.n)+self.p
+
+    return x1, x2, x3
 
 
-    def __getattr__(self, name):
+  def __getattr__(self, name):
     calcName = "_" + name
 
-    if hasattr(self, calcName):
-      getattr(self, calcName)()
-      return getattr(self, name)
-    else:
-      raise AttributeError
+  if hasattr(self, calcName):
+    getattr(self, calcName)()
+    return getattr(self, name)
+  else:
+    raise AttributeError
     
-    def _f(self):
+    
+  def _f(self):
     self.f = (3*self.c/self.a - self.b**2/self.a**2)/3
 
-    def _g(self):
+  def _g(self):
     self.g = (2*self.b**3/self.a**3 - 9*self.b*self.c/self.a**2 + 27*self.d/self.a)/27
 
-    def _h(self):
+  def _h(self):
     self.h = self.g**2/4 + self.f**3/27
 
-    def _r(self):
+  def _r(self):
     self.r = -(self.g/2) + self.h**0.5
-    return x1, x2, x3
+
+  def _s(self):
+    self.s = -abs(self.r)**(1/3) if self.r < 0 else self.r**(1/3)
+
+  def _t(self):
+    self.t = -(self.g/2) - self.h**0.5
+
+  def _u(self):
+    self.u = -abs(self.t)**(1/3) if self.t < 0 else self.t**(1/3)
+
+  def _i(self):
+    self.i = ((self.g**2/4)-self.h)**0.5
+
+  def _j(self):
+    self.j = self.i**(1/3)
